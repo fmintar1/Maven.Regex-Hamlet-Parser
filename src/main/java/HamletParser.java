32d22081b45122791573e8da3366828333f84b38
Freddy Mintarja <115741236+fmintar1@users.noreply.github.com>
2022-12-10 19:59:57 -0500

    Done

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by thook on 10/7/15.
@@ -13,6 +15,12 @@ public class HamletParser {
         this.hamletData = loadFile();
     }
 
+    public static void main(String[] args) {
+        HamletParser parser = new HamletParser();
+        System.out.println(parser.getHamletData().replaceAll("(?i)Hamlet","Leon"));
+        System.out.println(parser.getHamletData().replaceAll("(?i)Horatio","Tariq"));
+
+    }
     private String loadFile(){
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("hamlet.txt").getFile());
@@ -34,6 +42,12 @@ public class HamletParser {
 
     public String getHamletData(){
         return hamletData;
+    }
+
+    public String replaceText(String replace, String text, String replaceWith) {
+        Pattern p1 = Pattern.compile(replace);
+        Matcher m1 = p1.matcher(text);
+        return m1.replaceAll(replaceWith);
     }
 
 }
--- src/test/java/HamletParserTest.java
+++ src/test/java/HamletParserTest.java
@@ -1,6 +1,9 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import static org.junit.Assert.*;
 
 public class HamletParserTest {
@@ -15,17 +18,41 @@ public class HamletParserTest {
 
     @Test
     public void testChangeHamletToLeon() {
+        hamletText = hamletParser.replaceText("Hamlet", hamletText, "Leon");
+        hamletText = hamletParser.replaceText("HAMLET", hamletText, "LEON");
+        assertFalse(hamletText.contains("HAMLET"));
+        assertFalse(hamletText.contains("Hamlet"));
+        assertTrue(hamletText.contains("LEON"));
+        assertTrue(hamletText.contains("Leon"));
     }
 
     @Test
     public void testChangeHoratioToTariq() {
+        hamletText = hamletParser.replaceText("Horatio", hamletText, "Tariq");
+        hamletText = hamletParser.replaceText("HORATIO", hamletText, "TARIQ");
+        assertFalse(hamletText.contains("Horatio"));
+        assertFalse(hamletText.contains("HORATIO"));
+        assertTrue(hamletText.contains("Tariq"));
+        assertTrue(hamletText.contains("TARIQ"));
     }
 
     @Test
     public void testFindHoratio() {
+        assertTrue(hamletText.contains("Horatio"));
+        assertTrue(hamletText.contains("HORATIO"));
+        hamletText = hamletParser.replaceText("Horatio", hamletText, "");
+        hamletText = hamletParser.replaceText("HORATIO", hamletText, "");
+        assertFalse(hamletText.contains("Horatio"));
+        assertFalse(hamletText.contains("HORATIO"));
     }
 
     @Test
     public void testFindHamlet() {
+        assertTrue(hamletText.contains("Hamlet"));
+        assertTrue(hamletText.contains("HAMLET"));
+        hamletText = hamletParser.replaceText("Hamlet", hamletText, "");
+        hamletText = hamletParser.replaceText("HAMLET", hamletText, "");
+        assertFalse(hamletText.contains("Hamlet"));
+        assertFalse(hamletText.contains("HAMLET"));
     }
 }
